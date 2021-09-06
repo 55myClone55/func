912f1ccabd308c053b7a641e8a96858e7a097a14
55myClone55 <borismoskalyuk3@gmail.com>
2021-09-06 11:41:43 +0300

    func
    func

---
+++ fynkcional/src/components/data.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Cheese',
+            price: 120,
+            image: 'https://images.unsplash.com/photo-1608666565775-65cb1097627f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNoZWVzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+        },
+        {
+            id: '2',
+            name: 'Meat',
+            price: 130,
+            image: 'https://images.unsplash.com/photo-1603048374877-b98f840ad441?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWVhdCUyMGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+        },
+        {
+            id: '3',
+            name: 'Fish',
+            price: 100,
+            image: 'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZpc2glMjBmb29kfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+        },
+    ],
+};
+export default data;
